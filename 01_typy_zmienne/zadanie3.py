699b5895ced09e12fe10ea755b2e4bbad40d892d
leet23 <marek.dabrowski90@gmail.com>
2019-10-09 19:57:21 +0200

    lekcja

---
+++ 01_typy_zmienne/zadanie3.py
@@ -0,0 +1,13 @@
+# zadanie 3
+quote = "Honesty is the first chapter in the book of wisdom."
+print(quote)
+print(len(quote)) #1
+print(quote[-7:-1]) #2
+print(quote[0:25]) #3
+print(quote[0:len(quote)//2]) #3
+print(quote[-1]) #4
+print(quote[len(quote)//2 : : 3]) #5
+print(quote[ : : 2]) #6
+print(quote[ : : -1]) #7
+print(quote.replace("wisdom", "friendship")) #8
+print()
---
+++ 01_typy_zmienne/zadanie5.py
@@ -0,0 +1,11 @@
+# zadanie 5
+p='kajak'
+print(p)
+print(p == p[::-1])
+print()
+sen = "Kobyła ma mały bok"
+sen.replace(" ", "")
+sen = sen.replace(" ", "")
+sen.lower()
+print(sen)
+print(sen == sen [::-1])
