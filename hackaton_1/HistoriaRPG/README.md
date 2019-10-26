d402f6ae0a62f6577e88682eaed48cf86ae080bb
leet23 <marek.dabrowski90@gmail.com>
2019-10-26 15:52:05 +0200

    hackaton day

---
+++ hackaton_1/HistoriaRPG/README.md
@@ -0,0 +1,14 @@
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+This project is short stories RPG with random function.
+
+## Technologies
+Project is created with:
+* Python 3.7
+
+## Setup
+Just run !
--- hackaton_1/HistoriaRPG/historyjka.py
+++ hackaton_1/HistoriaRPG/historyjka.py
@@ -15,5 +15,6 @@ rand_p = random.choice(places)
 rand_a = random.choice(adventures)
 rand_r = random.choice(roles)
 
-story = "Once upon a time there was a " + rand_r + " whose name was " + rand_n + ". He followed the path that led to the " + rand_p + "."
+story = "Once upon a time there was a " + rand_r + " whose name was " + rand_n + \
+        ". He followed the path that led to the " + rand_p + ". An "+ rand_a +" awaits him here."
 print(story)
